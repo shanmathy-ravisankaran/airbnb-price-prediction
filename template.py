1968b05a53f8eb6dfb5e6a94b1327c09ef87946c
Mayank Arya <shanmathy99@gmail.com>
2024-04-21 10:00:00 +0000

    added

--- template.py
+++ template.py
@@ -29,3 +29,11 @@ list_of_files = [
     "Dockerfile"
 ]
 
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
